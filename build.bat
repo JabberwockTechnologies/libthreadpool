6f4135727df90504ee6ebf388d95cf850db409a1
Ian Henriksen <insertinterestingnamehere@gmail.com>
2022-04-21 22:55:23 -0600

    Add convenience build script.

---
+++ build.bat
@@ -0,0 +1,7 @@
+:: A convenience script for calling CMake.
+rmdir /s /q build
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release ..
+cmake --build . --config Release -- -clp:ShowCommandLine
+cd ..
