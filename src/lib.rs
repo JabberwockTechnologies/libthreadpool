13e11e239f6d9394ed6ff19aeb571b271d6b013b
Ian Henriksen <insertinterestingnamehere@gmail.com>
2022-06-04 12:09:14 -0600

    Add local tid functionality.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,13 @@
+
+// Thread ID in current work group.
+thread_local!(#[allow(non_upper_case_globals)] static libthreadpool_local_tid: i32 = i32::MAX);
+
 #[no_mangle]
-pub unsafe extern "C" fn multiply_by_two(val: u64) -> u64 {
-    return val * 2u64;
+pub unsafe extern "C" fn libthreadpool_get_local_tid() -> i32 {
+    let mut tid = 0i32;
+    libthreadpool_local_tid.with(|t| {
+        tid = *t;
+    });
+    return tid;
 }
+
